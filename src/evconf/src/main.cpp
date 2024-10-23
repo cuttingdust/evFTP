630625d912cd363005f8e466e91460e303120778
handabao <18862963156@163.com>
2024-10-23 16:43:08 +0800

    windows iocp 支持线程池操作

--- src/evconf/src/main.cpp
+++ src/evconf/src/main.cpp
@@ -1,10 +1,19 @@
 #include <event2/event.h>
 #include <event2/listener.h>
+#include <event2/thread.h>
 #include <iostream>
 
 #ifndef _WIN32
 #include <signal.h>
 #endif
+
+#define SPORT 5001
+
+void listen_cb(evconnlistener *ev, evutil_socket_t s, struct sockaddr *addr, int socklen, void *arg)
+{
+    std::cout << "lesson_cb" << std::endl;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -38,15 +47,26 @@ int main(int argc, char *argv[])
     ///设置网络模型
     // event_config_avoid_method(conf, "select");
     // event_config_avoid_method(conf, "wepoll");
-    event_config_avoid_method(conf, "kqueue");
+    // event_config_avoid_method(conf, "win32");
+    // event_config_avoid_method(conf, "kqueue");
     // event_config_avoid_method(conf, "epoll");
-    event_config_avoid_method(conf, "poll");
+    // event_config_avoid_method(conf, "poll");
 
+    /// windows 中支持IOCP (线程池)
+#ifdef _WIN32
+    event_config_set_flag(conf, EVENT_BASE_FLAG_STARTUP_IOCP);
+    /// 初始化iocp的线程
+    evthread_use_windows_threads();
+    /// 设置cpu数量
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    event_config_set_num_cpus_hint(conf, si.dwNumberOfProcessors);
+#endif
 
     /// 设置特征
     /// 设置了EV_FEATURE_FDS 其他特征就无法设置，在windows中EV_FEATURE_FDS无效
     /// 仅仅限制kqueue epoll
-    event_config_require_features(conf, EV_FEATURE_ET);
+    // event_config_require_features(conf, EV_FEATURE_ET);
     // event_config_require_features(conf, EV_FEATURE_ET | EV_FEATURE_FDS);
     // event_config_require_features(conf, EV_FEATURE_FDS);
 
@@ -94,8 +114,21 @@ int main(int argc, char *argv[])
             std::cout << "EV_FEATURE_EARLY_CLOSE events are not supported." << std::endl;
 
         std::cout << "event_base_new_with_config success!" << std::endl;
+
+
+        sockaddr_in sin;
+        memset(&sin, 0, sizeof(sin));
+        sin.sin_family = AF_INET;
+        sin.sin_port   = htons(SPORT);
+
+        evconnlistener *ev = evconnlistener_new_bind(
+                base, listen_cb, base, 10, LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE, (sockaddr *)&sin, sizeof(sin));
+
+        event_base_dispatch(base);
+        evconnlistener_free(ev);
         event_base_free(base);
     }
+
 
 #ifdef _WIN32
     WSACleanup();
