746de42e0e8c24bf2057837ea8e65e1166f8b64a
handabao <18862963156@163.com>
2024-11-04 10:42:43 +0800

    调用zlib 和 输入输出过滤器 压缩解压数据 减少网络开支

--- src/evbuffer_zlib_server/src/zlib_client.cpp
+++ src/evbuffer_zlib_server/src/zlib_client.cpp
@@ -3,34 +3,127 @@
 #include <event2/bufferevent.h>
 #include <event2/buffer.h>
 #include <iostream>
+#include <zlib.h>
 
 #define FILEPATH "001.txt"
 
 struct ClientStatus
 {
-    FILE* fp  = 0;
-    bool  end = false;
+    FILE*     fp        = 0;
+    bool      end       = false;
+    bool      startSend = false;
+    z_stream* z_output  = 0;
+    int       readNum   = 0;
+    int       sendNum   = 0;
+    ~ClientStatus()
+    {
+        if (fp)
+        {
+            fclose(fp);
+            fp = 0;
+        }
+        if (z_output)
+        {
+            deflateEnd(z_output);
+            delete z_output;
+            z_output = 0;
+        }
+        readNum = 0;
+        sendNum = 0;
+    }
 };
 
 bufferevent_filter_result filter_out(evbuffer* s, evbuffer* d, ev_ssize_t limit, bufferevent_flush_mode mode, void* arg)
 {
+    ClientStatus* sta = (ClientStatus*)arg;
     std::cout << "[Client]: "
               << "client filter_out" << std::endl;
-    char data[1024] = { 0 };
-    int  len        = evbuffer_remove(s, data, sizeof(data));
-    evbuffer_add(d, data, len);
+
+    /// 压缩文件
+    ///  发送文件消息001 去掉
+    if (!sta->startSend)
+    {
+        char data[1024] = { 0 };
+        int  len        = evbuffer_remove(s, data, sizeof(data));
+        evbuffer_add(d, data, len);
+        return BEV_OK;
+    }
+
+    /// 开始压缩文件
+    ///  取出buffer中数据的引用
+    evbuffer_iovec v_in[1];
+    int            n = evbuffer_peek(s, -1, 0, v_in, 1);
+    if (n <= 0)
+    {
+        /// 调用write 回调 清理空间
+        if (sta->end)
+        {
+            return BEV_OK;
+        }
+
+        /// 没有数据 BEV_NEED_MORE 不会进入写入回调
+        return BEV_NEED_MORE;
+    }
+    z_stream* p = sta->z_output;
+    if (!p)
+    {
+        return BEV_ERROR;
+    }
+    /// zlib 输入数据大小
+    p->avail_in = v_in[0].iov_len;
+    /// zlib  输入数据地址
+    p->next_in = (Byte*)v_in[0].iov_base;
+
+    /// 申请输出空间大小
+    evbuffer_iovec v_out[1];
+    evbuffer_reserve_space(d, 4096, v_out, 1);
+
+    /// zlib 输出空间大小
+    p->avail_out = v_out[0].iov_len;
+    /// zlib 输出空间地址
+    p->next_out = (Byte*)v_out[0].iov_base;
+
+    /// zlib 压缩
+    int ret = deflate(p, Z_SYNC_FLUSH);
+    if (ret != Z_OK)
+    {
+        std::cerr << "deflate failed!" << std::endl;
+    }
+
+
+    /// 压缩用了多少数据 从source evbuffer中移除
+    /// p->avail_in 未处理数据大小
+    size_t nread = v_in[0].iov_len - p->avail_in;
+
+    /// 压缩后数据大小 传入 des evbuffer
+    ///  p->avail_out 剩余空间大小
+    size_t nwrite = v_out[0].iov_len - p->avail_out;
+
+    /// 移除source evbuffer中数据
+    evbuffer_drain(s, nread);
+
+    /// 传入des evbuffer
+    v_out[0].iov_len = nwrite;
+    evbuffer_commit_space(d, v_out, 1);
+    std::cout << "[Client]: "
+              << "nread: " << nread << " nwrite: " << nwrite << std::endl;
+    sta->readNum += nread;
+    sta->sendNum += nwrite;
 
     return BEV_OK;
 }
 
 void client_read_cb(bufferevent* bev, void* arg)
 {
+    ClientStatus* sta = (ClientStatus*)arg;
+
     /// 002 接收服务端发送的OK回复
-    char data[1024] = { 0 };
-    int  len        = bufferevent_read(bev, data, sizeof(data) - 1);
+    char   data[1024] = { 0 };
+    size_t len        = bufferevent_read(bev, data, sizeof(data) - 1);
     if (strcmp(data, "OK") == 0)
     {
-        std::cout << data << std::endl;
+        std::cout << "[Client]: " << data << std::endl;
+        sta->startSend = true;
         /// 开始发送文件,触发写入回调
         bufferevent_trigger(bev, EV_WRITE, 0);
     }
@@ -38,7 +131,8 @@ void client_read_cb(bufferevent* bev, void* arg)
     {
         bufferevent_free(bev);
     }
-    std::cout << "client_read_cb " << len << std::endl;
+    std::cout << "[Client]: "
+              << "client_read_cb " << len << std::endl;
 }
 
 void client_write_cb(bufferevent* bev, void* arg)
@@ -54,15 +148,21 @@ void client_write_cb(bufferevent* bev, void* arg)
         bufferevent* be = bufferevent_get_underlying(bev);
         /// 获取输出缓冲及其大小
         evbuffer* evb = bufferevent_get_output(be);
-        int       len = evbuffer_get_length(evb);
+        size_t    len = evbuffer_get_length(evb);
         std::cout << "[Client]: " << __func__ << " "
                   << "evbuffer_get_length = " << len << std::endl;
 
         if (len <= 0)
         {
+            std::cout << "[Client]: "
+                      << "read " << s->readNum << " "
+                      << "send " << s->sendNum << std::endl;
+
             /// 立即清理 如果缓冲有数据, 不会发送
             bufferevent_free(bev);
             delete s;
+
+
             return;
         }
 
@@ -79,11 +179,10 @@ void client_write_cb(bufferevent* bev, void* arg)
     std::cout << "[Client]: "
               << "client_write_cb" << std::endl;
     /// 读取文件内容
-    char data[1024] = { 0 };
-    int  len        = fread(data, 1, sizeof(data), fp);
+    char   data[1024] = { 0 };
+    size_t len        = fread(data, 1, sizeof(data), fp);
     if (len <= 0)
     {
-        fclose(fp);
         s->end = true;
 
         /// 刷新缓冲区
@@ -105,9 +204,7 @@ void client_event_cb(bufferevent* bev, short events, void* arg)
         /// 001 发送文件名
         bufferevent_write(bev, FILEPATH, strlen(FILEPATH));
 
-        /// 创建输出过滤
-        bufferevent* bev_filter =
-                bufferevent_filter_new(bev, 0, filter_out, BEV_OPT_CLOSE_ON_FREE | BEV_OPT_DEFER_CALLBACKS, 0, 0);
+
         FILE* fp = fopen(FILEPATH, "rb");
         if (!fp)
         {
@@ -117,6 +214,13 @@ void client_event_cb(bufferevent* bev, short events, void* arg)
         }
         ClientStatus* s = new ClientStatus();
         s->fp           = fp;
+        s->z_output     = new z_stream();
+        deflateInit(s->z_output, Z_DEFAULT_COMPRESSION);
+
+
+        /// 创建输出过滤
+        bufferevent* bev_filter =
+                bufferevent_filter_new(bev, 0, filter_out, BEV_OPT_CLOSE_ON_FREE | BEV_OPT_DEFER_CALLBACKS, 0, s);
 
         /// 设置回调
         bufferevent_setcb(bev_filter, client_read_cb, client_write_cb, client_event_cb, s);
--- src/evbuffer_zlib_server/src/zlib_server.cpp
+++ src/evbuffer_zlib_server/src/zlib_server.cpp
@@ -2,19 +2,39 @@
 #include <event2/listener.h>
 #include <event2/bufferevent.h>
 #include <event2/buffer.h>
+#include <zlib.h>
 
 #include <iostream>
 #include <filesystem>
 
 #include <event2/buffer.h>
-#include <event2/bufferevent.h>
 
 #define SPORT 8080
 
 struct Status
 {
-    bool  start = false;
-    FILE *fp    = 0;
+    bool      start    = false;
+    FILE     *fp       = 0;
+    z_stream *p        = 0;
+    int       recvNum  = 0;
+    int       writeNum = 0;
+    ~Status()
+    {
+        if (fp)
+        {
+            fclose(fp);
+            fp = 0;
+        }
+
+        if (p)
+        {
+            inflateEnd(p);
+            delete p;
+            p = 0;
+        }
+        recvNum  = 0;
+        writeNum = 0;
+    }
 };
 
 
@@ -22,12 +42,72 @@ bufferevent_filter_result filter_in(struct evbuffer *src, struct evbuffer *dst, 
                                     enum bufferevent_flush_mode mode, void *arg)
 {
     std::cout << "[Server]: " << __func__ << std::endl;
+    Status *status = (Status *)arg;
     /// 1. 接受客户端发送的文件名
-    char data[1024] = { 0 };
-    int  len        = evbuffer_remove(src, data, sizeof(data) - 1);
+    if (!status->start)
+    {
+        char data[1024] = { 0 };
+        int  len        = evbuffer_remove(src, data, sizeof(data) - 1);
+        std::cout << "[Server]: "
+                  << "server recv " << data << std::endl;
+        evbuffer_add(dst, data, len);
+        return BEV_OK;
+    }
+
+    /// 解压
+    evbuffer_iovec v_in[1];
+    /// 读取数据 不清理缓冲
+    int n = evbuffer_peek(src, -1, nullptr, v_in, 1);
+    if (n <= 0)
+    {
+        return BEV_NEED_MORE;
+    }
+
+    /// 解压上下文
+    z_stream *p = status->p;
+    if (!p)
+    {
+        return BEV_ERROR;
+    }
+    /// zlib 输入数据大小
+    p->avail_in = v_in[0].iov_len;
+    /// zlib  输入数据地址
+    p->next_in = (Byte *)v_in[0].iov_base;
+
+    /// 申请输出空间大小
+    evbuffer_iovec v_out[1];
+    evbuffer_reserve_space(dst, 4096, v_out, 1);
+
+    /// zlib 输出空间大小
+    p->avail_out = v_out[0].iov_len;
+    /// zlib 输出空间地址
+    p->next_out = (Byte *)v_out[0].iov_base;
+
+    int re = inflate(p, Z_SYNC_FLUSH);
+    if (re != Z_OK)
+    {
+        std::cerr << "deflare failed!" << std::endl;
+    }
+
+    /// 解压用了多少数据 从source evbuffer中移除
+    /// p->avail_in 未处理数据大小
+    size_t nread = v_in[0].iov_len - p->avail_in;
+
+    /// 解压后数据大小 传入 des evbuffer
+    ///  p->avail_out 剩余空间大小
+    size_t nwrite = v_out[0].iov_len - p->avail_out;
+
+    /// 移除source evbuffer中数据
+    evbuffer_drain(src, nread);
+
+    /// 传入des evbuffer
+    v_out[0].iov_len = nwrite;
+    evbuffer_commit_space(dst, v_out, 1);
     std::cout << "[Server]: "
-              << "server recv " << data << std::endl;
-    evbuffer_add(dst, data, len);
+              << "nread: " << nread << " nwrite: " << nwrite << std::endl;
+
+    status->recvNum += nread;
+    status->writeNum += nwrite;
 
     return BEV_OK;
 }
@@ -70,36 +150,40 @@ void read_cb(bufferevent *bev, void *arg)
     do
     {
         /// 写入文件
-        char data[1024] = { 0 };
-        int  len        = bufferevent_read(bev, data, sizeof(data));
+        char   data[1024] = { 0 };
+        size_t len        = bufferevent_read(bev, data, sizeof(data));
         if (len >= 0)
         {
             fwrite(data, 1, len, status->fp);
             fflush(status->fp);
         }
-    } while (evbuffer_get_length(bufferevent_get_input(bev)) > 0);
-    
+    }
+    while (evbuffer_get_length(bufferevent_get_input(bev)) > 0);
 }
 
 void event_cb(bufferevent *bev, short events, void *arg)
 {
-    std::cout << "event_cb" << std::endl;
+    std::cout << "[Server]: "
+              << "event_cb" << std::endl;
     Status *status = (Status *)arg;
     if (events & BEV_EVENT_EOF)
     {
-        std::cout << "[Server]: " <<  "connection closed" << std::endl;
+        std::cout << "[Server]: "
+                  << "connection closed" << std::endl;
+        std::cout << "[Server]: "
+                  << "recv " << status->recvNum << " "
+                  << "write " << status->writeNum << std::endl;
         /// 清理资源
-        if (status->fp)
+        if (status)
         {
-            fclose(status->fp);
-            status->fp = 0;
+            delete status;
         }
         bufferevent_free(bev);
     }
     else if (events & BEV_EVENT_ERROR)
     {
         std::cout << "[Server]: "
-                            << "some other error" << std::endl;
+                  << "some other error" << std::endl;
     }
     else if (events & BEV_EVENT_CONNECTED)
     {
@@ -117,6 +201,8 @@ void listen_cb(struct evconnlistener *e, evutil_socket_t s, struct sockaddr *a, 
     /// 1. 创建一个bufferevent 用来通信
     bufferevent *bev    = bufferevent_socket_new(base, s, BEV_OPT_CLOSE_ON_FREE);
     Status      *status = new Status();
+    status->p           = new z_stream();
+    inflateInit(status->p);
 
     /// 2. 添加过滤 并设置输入回调
     bufferevent *bev_filter = bufferevent_filter_new(bev,                   /// bufferevent
