772cfd72a2481b5af198b52964453688d43a7255
handabao <18862963156@163.com>
2024-11-06 16:53:26 +0800

    清理资源和超时判断

--- src/evthreadpool/src/XFTPServerCMD.cpp
+++ src/evthreadpool/src/XFTPServerCMD.cpp
@@ -5,6 +5,21 @@
 
 #include <iostream>
 
+void event_cb(struct bufferevent* bev, short what, void* ctx)
+{
+    std::cout << "event_cb" << std::endl;
+    auto* cmd = (XFTPServerCMD*)ctx;
+
+    /// 如果对方网络断掉，或者机器死机有可能收不到BEV_EVENT_EOF数据
+    if (what & (BEV_EVENT_EOF | BEV_EVENT_ERROR | BEV_EVENT_TIMEOUT))
+    {
+        std::cout << "BEV_EVENT_EOF | BEV_EVENT_ERROR | BEV_EVENT_TIMEOUT" << std::endl;
+        bufferevent_free(bev);
+        delete cmd;
+    }
+}
+
+
 static void read_cb(bufferevent* bev, void* arg)
 {
     auto* cmd        = (XFTPServerCMD*)arg;
@@ -42,7 +57,12 @@ auto XFTPServerCMD::init() -> bool
     /// base socket
 
     bufferevent* bev = bufferevent_socket_new(base_, sock_, BEV_OPT_CLOSE_ON_FREE);
-    bufferevent_setcb(bev, read_cb, 0, 0, this);
+    bufferevent_setcb(bev, read_cb, 0, event_cb, this);
     bufferevent_enable(bev, EV_READ | EV_WRITE);
+
+    /// 添加超时
+    timeval rt_ = { 10, 0 };
+    bufferevent_set_timeouts(bev, &rt_, 0);
+
     return true;
 }
